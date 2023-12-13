6d13ef2f2507ef7b8afa46040dac3ebdefed3717
melike1313 <melikenogay13@gmail.com>
2023-12-13 13:50:50 +0300

    server

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const http = require('http')
 
 const server = http.createServer((req,res)=>{
-    console.log("User hit the server")
-    res.end('home page')
+    res.writeHead(200,{'content-type':'text/html'})
+    res.write('<h1>home page</h1>')
 })
 
 server.listen(5000)
