121dd958224b7e9d004e3589e992ff94489f8bc0
melike1313 <melikenogay13@gmail.com>
2023-12-14 20:26:04 +0300

    req

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const http = require('http')
 
 const server = http.createServer((req,res)=>{
-   // console.log(req.method)
-   console.log(req.url)
+     // console.log(req.method)
+    // console.log(req.url)
+    const url =req.url
+    if(url==='/'){
+        res.writeHead(200,{'content-type':'text/html'})
+        res.write('<h1>home page</h1>')
+        res.end()
+    }
+    else if(url==='/about'){
     res.writeHead(200,{'content-type':'text/html'})
-    res.write('<h1>home page</h1>')
-    res.end()
-})
+    res.write('<h1>about page</h1>')
+    res.end()}
+    else{
+        res.writeHead(404,{'content-type':'text/html'})
+        res.write('<h1>page not found</h1>')
+        res.end()}
+});
 
 server.listen(5000)
