bdd548804e418d451bf9d10a48e49eef3735e5a3
melike1313 <melikenogay13@gmail.com>
2023-12-14 21:12:33 +0300

    req

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const http = require('http')
+const http = require("http");
 
-const server = http.createServer((req,res)=>{
-     // console.log(req.method)
-    // console.log(req.url)
-    const url =req.url
-    if(url==='/'){
-        res.writeHead(200,{'content-type':'text/html'})
-        res.write('<h1>home page</h1>')
-        res.end()
-    }
-    else if(url==='/about'){
-    res.writeHead(200,{'content-type':'text/html'})
-    res.write('<h1>about page</h1>')
-    res.end()}
-    else{
-        res.writeHead(404,{'content-type':'text/html'})
-        res.write('<h1>page not found</h1>')
-        res.end()}
+const server = http.createServer((req, res) => {
+  // console.log(req.method)
+  // console.log(req.url)
+  const url = req.url;
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>home page</h1>");
+    res.end();
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>about page</h1>");
+    res.end();
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>page not found</h1>");
+    res.end();
+  }
 });
 
-server.listen(5000)
+server.listen(5000);
