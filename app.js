035986bb954b0e2d46258a6f7d939e6141d97fe0
melike1313 <melikenogay13@gmail.com>
2023-12-16 21:47:24 +0300

    origin

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/',(req,res)=>{
-  console.log('console hit the resource')
-res.status(200).send('Home page')
-})
+app.get("/", (req, res) => {
+  console.log("console hit the resource");
+  res.status(200).send("Home page");
+});
 
+app.get("/about", (req, res) => {
+  console.log("console hit the resource");
+  res.status(200).send("About page");
+});
 
-app.get('/about',(req,res)=>{
-  console.log('console hit the resource')
-res.status(200).send('About page')
-})
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>Resource not found</h1>");
+});
 
-app.all('*', (req, res) => {
-  res.status(404).send('<h1>Resource not found</h1>')
-})
-
-app.listen(5000,()=>{
-  console.log('port 5000 is listening')
-})
+app.listen(5000, () => {
+  console.log("port 5000 is listening");
+});
