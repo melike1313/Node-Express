d80bd5278903585367f955537fc2549370af3d50
melike1313 <melikenogay13@gmail.com>
2023-12-16 21:38:55 +0300

    origin

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require('express')
 const app = express()
+
+app.get('/',(req,res)=>{
+  console.log('console hit the resource')
+res.send('Home page')
+})
+
+
+app.get('/about',(req,res)=>{
+  console.log('console hit the resource')
+res.send('About page')
+})
+
+
+
 app.listen(5000,()=>{
   console.log('port 5000 is listening')
 })
