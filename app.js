0a191419129acb49926ca1830d32e461e1af3cae
melike1313 <melikenogay13@gmail.com>
2023-12-19 12:12:50 +0300

    update

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express();
 
-app.listen(5000,(req,res)=>{
+app.get('/',(req,res)=>{
+  res.json([{name:'melike'}])
+})
+
+app.listen(5000,()=>{
   console.log('Server is listening on port 5000...')
 
 })
