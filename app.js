335f2e044c6a709b46ad45c04f9eaaae80be03f9
melike1313 <melikenogay13@gmail.com>
2023-12-19 12:00:21 +0300

    update

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
-const express = require("express");
-const path = require("path");
+const express = require('express')
 const app = express();
 
-// setup static and middleware
-app.use(express.static("./public"));
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-});
-app.get("*", (req, res) => {
-  res.status(404).send("resource not found");
-});
+app.listen(5000,(req,res)=>{
+  console.log('Server is listening on port 5000...')
 
-app.listen(5000, () => {
-  console.log(`Server listening on port 5000`);
-});
+})
