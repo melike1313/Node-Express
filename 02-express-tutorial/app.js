2146da4e9601c3089655dc62f54866588cc3a055
melike1313 <melikenogay13@gmail.com>
2023-12-23 11:21:42 +0300

    new

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -5,6 +5,7 @@ const { products } = require("./data");
 app.get("/", (req, res) => {
   res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
 });
+
 app.listen(5000, () => {
   console.log("Server 5000 are listening ...");
 });
