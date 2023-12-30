d9362a2c743c91db3df1f71203fccac33a6d20ca
melike1313 <melikenogay13@gmail.com>
2023-12-30 11:18:35 +0300

    new

--- app.js
+++ app.js
@@ -6,53 +6,53 @@ app.get("/", (req, res) => {
   res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
 });
 
-app.get("/api/products",(req,res)=>{
-  const newProducts = products.map((product)=>{
-    const{id,name,image}=product;
-    return{id,name,image}
-  })
-  res.json(newProducts)
-})
-app.get("/api/v1/query",(req,res)=>{
-  const {search,limit}=req.query
-  let sortedProducts=[...products]
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
+app.get("/api/v1/query", (req, res) => {
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
 
-  if(search){
-    sortedProducts=sortedProducts.filter((product)=>{
-      return product.name.startsWith(search)
-    })
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
   }
-  if(limit)
-  {
-    sortedProducts=sortedProducts.slice(0,Number(limit))
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
   }
-  if(sortedProducts.length<1){
+  if (sortedProducts.length < 1) {
     // res.status(200).send("no product matched you")
-    return res.status(200).json({success:true,data:[]})
+    return res.status(200).json({ success: true, data: [] });
   }
-  res.status(200).json(sortedProducts)
-})
-app.get("/api/products/:productID",(req,res)=> {
+  res.status(200).json(sortedProducts);
+});
+app.get("/api/products/:productID", (req, res) => {
   // console.log(req)
   // console.log(req.params)
-  const {productID}=req.params;
-  const singleProduct = products.find((product)=>product.id===Number(productID))
-if(!singleProduct){
-  return res.status(404).send("The Product Not Found ")
-}
- return res.json(singleProduct)
-}
-)
+  const { productID } = req.params;
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send("The Product Not Found ");
+  }
+  return res.json(singleProduct);
+});
 
-app.get("/api/products/:productID/reviews/:reviewID",(req,res)=> {
-  console.log(req.params)
-  res.send('hello world')
-})
+app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
+  console.log(req.params);
+  res.send("hello world");
+});
 
-app.get("/api/products/2",(req,res)=>{
-  const secondProduct=products.find((product)=>product.id===2)
-  res.json(secondProduct)
-})
+app.get("/api/products/2", (req, res) => {
+  const secondProduct = products.find((product) => product.id === 2);
+  res.json(secondProduct);
+});
 
 app.listen(5000, () => {
   console.log("Server 5000 are listening ...");
