ae1f4ae627e13babb1675a7ef3f871d59506611c
melike1313 <melikenogay13@gmail.com>
2023-12-30 11:18:56 +0300

    new

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.get("/", (req, res) => {
   res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
 });
 
+
 app.get("/api/products", (req, res) => {
   const newProducts = products.map((product) => {
     const { id, name, image } = product;
